1a64564deb53cc93fca2625456ad5eba5f7d7e3d
RiveaDev <berkkaslan41@gmail.com>
2026-01-23 03:09:17 +0300

    Update counter.js

--- functions/counter.js
+++ functions/counter.js
@@ -1,1 +1,14 @@
+export async function onRequest({ env }) {
+  const key = "total";
+
+  let total = await env.COUNTER.get(key);
+  total = total ? Number(total) + 1 : 1;
+
+  await env.COUNTER.put(key, total.toString());
+
+  return new Response(
+    JSON.stringify({ total }),
+    { headers: { "Content-Type": "application/json" } }
+  );
+}
 
